25759619eead79ce36c34ac015799b57a8333bb0
William A <76547127+TANK1-41@users.noreply.github.com>
2022-06-03 00:05:12 -0400

    premake

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "Hazel_copy"
+	architecture"x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.build}-%{cfg.system}-%{cfg.architecture}"
+	
+project "Hazel_copy"
+	location "Hazel_copy"
+	kind "SharedLib"
+	language "C++"
+	
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vender/spdlog/include",
+		"%{prj.name}/src"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+		
+	filter "configurations:Debug"
+	    defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+	    defines "HZ_Release"
+		optimize  "On"
+
+	filter "configurations:Dist"
+	    defines "HZ_Dist"
+		optimize  "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel_copy/vender/spdlog/include",
+		"Hazel_copy/src",
+	}
+
+	links
+	{
+		"Hazel_copy"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+	    defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+	    defines "HZ_Release"
+		optimize  "On"
+
+	filter "configurations:Dist"
+	    defines "HZ_Dist"
+		optimize  "On"
+	
